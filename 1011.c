2617b2e31ce781cc9dde785e92f86f47f33fddb7
Henrique Ferreira <79875791+ferreiradh@users.noreply.github.com>
2021-04-07 23:21:34 -0300

    Create 1011.c

---
+++ 1011.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() { 
+    double raio, volume, pi = 3.14159;
+
+    scanf("%lf", &raio);
+    volume = (4.0 / 3) * pi * pow(raio, 3.0);
+    printf("VOLUME = %.3f\n", volume);
+
+    return 0;
+}
