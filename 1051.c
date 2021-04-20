69be389f586881acd94fa779e745e2eae69161ff
Henrique Ferreira <79875791+ferreiradh@users.noreply.github.com>
2021-04-20 14:46:08 -0300

    Update 1051.c

--- 1051.c
+++ 1051.c
@@ -2,42 +2,17 @@
 
 int main(){
 
-float sal, sal2, sal3;
+float sal;
 
     scanf("%f", &sal);
 
-    if(sal <= 2000){
-        printf("Isento\n");
+    if(sal <= 2000) printf("Isento\n");
 
-    }else
-    if(sal <= 3000){
-        sal = sal - 2000;
-        sal = sal * 0.08;
-        printf("R$ %.2f\n", sal);
+    else if(sal <= 3000) printf("R$ %.2f\n", (sal - 2000) * 0.08);
 
-    }else
-    if(sal <= 4500){
-        sal2 = sal - 3000;
-        sal = sal - 2000;
-        sal = sal - sal2;
-        sal = sal * 0.08;
-        sal2 = sal2 * 0.18;
+    else if(sal <= 4500) printf("R$ %.2f\n", ((sal - 3000) * 0.18) + 80);
 
-        printf("R$ %.2f\n", sal+sal2);
-
-    }else{
-        sal3 = sal - 4500;
-        sal2 = sal - 3000;
-        sal2 = sal2 - sal3;
-        sal = sal - 2000;
-        sal = (sal - sal2) - sal3;
-        sal = sal * 0.08;
-        sal2 = sal2 * 0.18;
-        sal3 = sal3 * 0.28;
-
-        printf("R$ %.2f\n", sal+sal2+sal3);
-    }
-
+    else printf("R$ %.2f\n", ((sal - 4500) * 0.28) + 350);
 
     return 0;
 
